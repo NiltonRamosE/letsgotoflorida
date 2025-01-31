407d802f362234a67625fff3f1604421d89883e7
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-01-31 16:30:45 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className="text-green-300 font-bold text-3xl mt-4 underline">Let's go to Florida!</h1>
+    </div>
+  );
+}
