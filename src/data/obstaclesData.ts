4dd397c8548c95b8f8c65858a0c27c83c03bb073
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-02-01 16:10:06 -0500

    refactor: change points objetcs

--- src/data/obstaclesData.ts
+++ src/data/obstaclesData.ts
@@ -1,7 +1,7 @@
 const obstaclesData = [
-  { image: "/obstacles/eve.jpg", points: -5 },
-  { image: "/obstacles/meewis.webp", points: -10 },
-  { image: "/obstacles/ansita.jpg", points: -7 },
+  { image: "/obstacles/eve.jpg", points: -10 },
+  { image: "/obstacles/meewis.webp", points: -20 },
+  { image: "/obstacles/ansita.jpg", points: -10 },
 ];
 
 export default obstaclesData;
--- src/data/rewardsData.ts
+++ src/data/rewardsData.ts
@@ -1,8 +1,8 @@
 const rewardsData = [
-  { image: "/rewards/twinkies.webp", points: 3 },
+  { image: "/rewards/twinkies.webp", points: 5 },
   { image: "/rewards/reeses.webp", points: 5 },
-  { image: "/rewards/mm.webp", points: 4 },
-  { image: "/rewards/hershey.webp", points: 8 },
+  { image: "/rewards/mm.webp", points: 5 },
+  { image: "/rewards/hershey.webp", points: 5 },
   { image: "/rewards/caprisun.webp", points: 10 },
 ];
 
